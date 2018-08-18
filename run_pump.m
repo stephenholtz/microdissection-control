d46084737286d092c7ae7d47aa2ee0f20d6e0ac8
Stephen L Holtz <stephenlholtz@gmail.com>
2018-08-18 19:15:18 -0400

    updates to run_pump script

--- run_pump.m
+++ run_pump.m
@@ -13,18 +13,18 @@ D = pump.daq();
 %% configure/run dissection
 
 % Set number of shuttered pulses (stabilization time)
-D.nShutteredPulses = 100;
+D.nShutteredPulses = 150;
 % Set number of delivered (unshuttered) pulses
-D.nDeliveredPulses = 100;
+D.nDeliveredPulses = 40;
 % Set the frequency of pulses (60-100Hz)
 D.pulseFrequency = 100;
 % Set duration of the N2 purge prior to lasing (10 seconds is OK)
-D.purgeDurSeconds = 10;
+D.purgeDurSeconds = 5;
 
 % Prompt manual entry into laser control software
 config_complete = pump.initGAMDlgs(D.nDeliveredPulses + D.nShutteredPulses);
 if config_complete
-    % It sometimes takes >10 seconds to be ready to fire
-    pause(12-D.purgeDurSeconds)
+    % It sometimes takes ~10 seconds to be ready to fire
+    pause(8-D.purgeDurSeconds)
     dataIn = D.runDissection();
 end
