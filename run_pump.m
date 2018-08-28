5573fa1ec32bae49d9e38e244d05d2b80d868af9
Stephen L Holtz <stephenlholtz@gmail.com>
2018-08-28 14:05:41 -0400

    running commit

--- run_pump.m
+++ run_pump.m
@@ -1,11 +1,10 @@
 %% Set up workspace
 clear all force; %#ok<*CLALL>
-close all force;
+close all force; clc;
 % Assert checkpoints for manual configuration steps
 pump.initWarningDlgs()
 
 %% Initalize national instruments daq interface
-clear all; close all; clc;
 L = pump.LaserIO();
 
 %% Open up ROI/Camera GUI
@@ -14,10 +13,10 @@ L = pump.LaserIO();
 %% configure/run dissection
 
 % Set number of shuttered pulses (stabilization time)
-L.nShutteredPulses = 150;
+L.nShutteredPulses = 200;
 % Set number of delivered (unshuttered) pulses
-L.nDeliveredPulses = 400;
-% Set the frequency of pulses (60-100Hz)
+L.nDeliveredPulses = 30;
+% Set the frequency of pulses (60+ Hz for good closed loop)
 L.pulseFrequency = 100;
 % Set duration of the N2 purge prior to lasing (10 seconds is OK)
 L.purgeDurSeconds = 5;
