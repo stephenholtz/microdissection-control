a8f7a26b17bee9dba36fa28d299c9114f8d16a47
Stephen L Holtz <stephenlholtz@gmail.com>
2018-10-30 18:57:31 -0400

    final touches

--- run_pump.m
+++ run_pump.m
@@ -1,6 +1,6 @@
 %% Initalize system
 clear all force; close all force; clc; %#ok<*CLALL>
-set(0,'DefaultFigureWindowStyle','normal');
+set(0,'DefaultFigureWindowStyle','docked');
 % Assert checkpoints for manual configuration steps
 pump.initWarningDlgs(); 
 
@@ -10,10 +10,13 @@ L = pump.LaserIO();
 % Open up ROI/Camera GUI
 use_roi_gui = 1;
 if use_roi_gui; R = pump.roiGUI(); end %#ok<*UNRCH>
+savepath = 'Z:\Wilson Lab\holtz\pump_prep_images\';
+
+%% Snap a picture
+if use_roi_gui; R.saveImg(fullfile(savepath,[datestr(now,30) '_image'])); end
 
 %% configure/run dissection
-savepath = 'Z:\Wilson Lab\holtz\pump_prep_images\';
-currtime = datestr(now,30);
+dissection_time = datestr(now,30);
 
 % Set number of shuttered pulses (stabilization time)
 L.nShutteredPulses = 250;
@@ -25,12 +28,12 @@ L.pulseFrequency = 100; % 60-100Hz is stable for closed loop / ext trig
 L.purgeDurSeconds = 5;
 
 % Prompt manual entry into laser control software
-if use_roi_gui; R.saveImg(fullfile(savepath,[currtime '_pre_dissection'])); end
+if use_roi_gui; R.saveImg(fullfile(savepath,[dissection_time '_pre_dissection'])); end
 config_complete = pump.initGAMDlgs(L.nDeliveredPulses + L.nShutteredPulses);
 if config_complete
     [dataIn,dataOut] = L.runDissection();
 end
 
 %% Save notes / data on server for documentation
-save(fullfile(savepath, ['data_' currtime '.mat']),'dataIn','-v7')
-copyfile('C:\Users\user\Desktop\temp_notes.txt',fullfile(savepath,['notes_' currtime '.txt']));
+save(fullfile(savepath, ['data_' dissection_time '.mat']),'dataIn','-v7')
+copyfile('C:\Users\user\Desktop\temp_notes.txt',fullfile(savepath,['notes_' dissection_time '.txt']));
