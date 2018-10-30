84d088f0f168c995f46f1b06594da8843fbdd85f
Stephen L Holtz <stephenlholtz@gmail.com>
2018-10-30 18:43:07 -0400

    add working simple preview

--- +pump/roiGUI.m
+++ +pump/roiGUI.m
@@ -1,8 +1,9 @@
 classdef roiGUI
     properties
-        camDev
-        displayTimer
-        frame
+        vidDev % video device adapter
+        hImage % image object of preview
+        figH % figure handle
+        cax % axes for camera preview
     end
 
     properties (Constant)
@@ -10,63 +11,56 @@ classdef roiGUI
         adaptorName = 'pointgrey';
         deviceID = 1;
         vidFormat = 'F7_Mono8_1024x1024_Mode1';
-        FrameRate = 30;
         
-        % GUI properties
-        displayRateHz = 30;
+        % getselectedsource() settings
+        FrameRate = 25;
+        Gamma = 1;
+        Shutter = 20;
         
+        roi_x = 512; % display x in middle of x
+        roi_y = 512; % display x in middle of y
         um_per_pix_mode0 = 10/214.7; % 
         um_per_pix_mode2 = 10/107.3; %
     end
     
     methods
         function obj = roiGUI()
-            % Start camera interface
             obj = obj.initCam();
-            
-            % Start GUI 
-            %obj = obj.initGUI();
-            
+            obj = obj.initGUI();
         end
         
         function obj = initCam(obj)
             % Create camera interface
             fprintf('Initializing PtGrey camera... ');
-            
-            % Clear the air a bit
             delete(imaqfind);
             imaqreset;
+            obj.vidDev = videoinput(obj.adaptorName,...
+                                    obj.deviceID, obj.vidFormat);
             
-            obj.camDev = imaq.VideoDevice(obj.adaptorName,obj.deviceID,obj.vidFormat);
-            obj.camDev.DeviceProperties.FrameRate = obj.FrameRate;
+            % use this command to configure settings
+            src = getselectedsource(obj.vidDev);
+            src.FrameRate = obj.FrameRate;
+            src.Gamma = obj.Gamma;
+            src.Shutter = obj.Shutter;
             
             fprintf('Done\n');
         end
         
         function obj = initGUI(obj)
-            fprintf('Initializing PtGrey camera... ');
-            figure;
-            
-            % Create timer for refreshing GUI with camera grabs
-            delete(timerfindall); delete(timerfindall);
-
-            obj.displayTimer = timer();
-            obj.displayTimer.StartDelay = 0; % Start asap
-            obj.displayTimer.BusyMode = 'drop';
-            obj.displayTimer.Period = round(1/obj.displayRateHz,3);
-            obj.displayTimer.ExecutionMode = 'fixedRate';
-            obj.displayTimer.TimerFcn = @obj.newFrame;
-            start(obj.displayTimer)
-            
-            fprintf('Done\n');
+            obj.figH = figure('Name', 'PUMP GUI',...
+                              'NumberTitle', 'off');
+            obj.cax = axes(obj.figH);
+            obj.cax.Box = 'off';
+            frame = getsnapshot(obj.vidDev);
+            obj.hImage = imshow(frame,[],...
+                                'InitialMagnification','fit',...
+                                'Parent',obj.cax);
+            preview(obj.vidDev, obj.hImage); hold on
+            plot(obj.roi_x,obj.roi_y,'c+','MarkerSize',14,'LineWidth',2,'Parent',obj.cax);
         end
         
-        function obj = newFrame(obj,~,~)
-            obj.frame = step(obj.camDev);
-        end
-        
-        function obj = saveImg(obj,filepath)
-            
+        function obj = saveImg(obj,~)
+            imsave(obj.frame)
         end
     end
 end
--- run_pump.m
+++ run_pump.m
@@ -1,17 +1,16 @@
-%% Set up workspace
-clear all force; %#ok<*CLALL>
-close all force; clc;
+%% Initalize system
+clear all force; close all force; clc; %#ok<*CLALL>
+set(0,'DefaultFigureWindowStyle','normal');
 % Assert checkpoints for manual configuration steps
-pump.initWarningDlgs()
+pump.initWarningDlgs(); 
 
-%% Initalize national instruments daq interface
+% Initalize daq interface
 L = pump.LaserIO();
 
-%% Open up ROI/Camera GUI
-use_roi_gui = 0;
-if use_roi_gui
-    R = pump.roiGUI(); %#ok<*UNRCH>
-end
+% Open up ROI/Camera GUI
+use_roi_gui = 1;
+if use_roi_gui; R = pump.roiGUI(); end %#ok<*UNRCH>
+
 %% configure/run dissection
 savepath = 'Z:\Wilson Lab\holtz\pump_prep_images\';
 currtime = datestr(now,30);
@@ -19,21 +18,16 @@ currtime = datestr(now,30);
 % Set number of shuttered pulses (stabilization time)
 L.nShutteredPulses = 250;
 % Set number of delivered (unshuttered) pulses
-L.nDeliveredPulses = 215;
+L.nDeliveredPulses = 190;
 % Set the frequency of pulses
 L.pulseFrequency = 100; % 60-100Hz is stable for closed loop / ext trig
-% Set duration of the N2 purge prior to lasing (10 seconds is OK)
+% Set duration of the N2 purge prior to lasing (5 seconds is OK)
 L.purgeDurSeconds = 5;
 
 % Prompt manual entry into laser control software
-if use_roi_gui
-    savename = [currtime '_pre_dissection'];
-    R.saveImg(fullfile(savepath,savename))
-end
-config_complete = pump.initGAMDlgs(L.nDeliveredPulses + ...
-                                    L.nShutteredPulses);
+if use_roi_gui; R.saveImg(fullfile(savepath,[currtime '_pre_dissection'])); end
+config_complete = pump.initGAMDlgs(L.nDeliveredPulses + L.nShutteredPulses);
 if config_complete
-    % daq data in/out for debugging
     [dataIn,dataOut] = L.runDissection();
 end
 
