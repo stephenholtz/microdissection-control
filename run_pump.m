82cbed2c74e9a3aebf3f8ca4e217c9d1068327c7
Stephen Holtz <stephenlholtz@gmail.com>
2023-01-31 11:45:20 -0800

    update running script

--- run_pump.m
+++ run_pump.m
@@ -5,25 +5,27 @@ clear all force; close all force; clc; %#ok<*CLALL>
 if ~pump.initWarningDlgs(); return; end
 L = pump.LaserIO('Dev3','B:\pump_prep_data_images\');
 
-% Open up ROI/Camera GUI (now not working)
-% R.saveImg(fullfile(savepath,[datestr(now,30) '_image']));
-
-%% configure/run dissection
-L.nDeliveredPulses = 110;       % number of dissection pulses (w/ beam shutter open)
-
-% Usually unchanged:
+% Constant dissection settings
 L.energyLevelMiliJoules = 8;    % manually entered in GAM software, this sets prompt text
 L.nShutteredPulses = 200;       % number of shuttered pulses, a for energy stabilization
 L.pulseFrequency = 100;         % laser pulse freq, >60Hz is stable for closed loop mode
 L.purgeDurSeconds = 3;          % duration of N2 purge prior to lasing (3+ seconds is OK)
 
-% Prompt manual entry into laser control software
-dissection_time = datestr(now,30);
-% R.saveImg(fullfile(L.savepath,[dissection_time '_pre_dissection']));
+%% Run a test cut on thorax for centering cut
+L.nDeliveredPulses = 200;       % number of dissection pulses (w/ beam shutter open)
 config_complete = pump.initGAMDlgs(L.nDeliveredPulses + L.nShutteredPulses,L.energyLevelMiliJoules);
-if config_complete
-    [dataIn,dataOut] = L.runDissection();
-end
+if config_complete; [dataInTestCut,dataOutTestCut] = L.runDissection(); end
+
+%% Cut into head capsule to help remove muscles + improve superfusion
+L.nDeliveredPulses = 400;       % number of dissection pulses (w/ beam shutter open)
+config_complete = pump.initGAMDlgs(L.nDeliveredPulses + L.nShutteredPulses,L.energyLevelMiliJoules);
+if config_complete; [dataInHeadCut,dataOutHeadCut] = L.runDissection(); end
+
+%% Antenna dissection. See notes for numbers, should be at least 75
+L.nDeliveredPulses = 115;       % number of dissection pulses (w/ beam shutter open)
+config_complete = pump.initGAMDlgs(L.nDeliveredPulses + L.nShutteredPulses,L.energyLevelMiliJoules);
+if config_complete; [dataInA2Cut,dataOutA2Cut] = L.runDissection(); end
 
 %% Save notes / data on server for documentation
-save(fullfile(L.savepath, ['data_' dissection_time '.mat']),'dataIn','-v7')
+save(fullfile(L.savepath, ['data_' dissection_time '.mat']), ...
+    "dataInTestCut","dataInA2Cut","dataInHeadCut",'-v7')
